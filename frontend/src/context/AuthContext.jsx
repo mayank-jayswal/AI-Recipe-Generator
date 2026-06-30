b2a324e6df9a3b821f696e70d194e7bb8c944d64
mayank-jayswal <jayswalmayank05@gmail.com>
2026-06-30 10:49:28 +0530

    fix: set initial loading state to true in AuthProvider to fix isssue

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -13,7 +13,7 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
        const token = localStorage.getItem("token");
